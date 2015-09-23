1c49ea00b7d1cc30b49792870a561bd58d1bb986
David Carlos <ddavidcarlos1392@gmail.com>
2015-09-23 15:53:48 -0300

    Install mosquitto package using Vagrant

---
+++ cookbooks/mosquitto/recipes/default.rb
@@ -0,0 +1,22 @@
+mosquitto_url = "http://repo.mosquitto.org/debian/"
+mosquitto_repo = "mosquitto-repo"
+
+apt_package 'curl'
+
+execute 'mosquitto:repo' do
+  command "curl -O #{mosquitto_url}#{mosquitto_repo}.gpg.key"
+  command "apt-key add #{mosquitto_repo}.gpg.key"
+  command "rm #{mosquitto_repo}.gpg.key"
+  command "cd /etc/apt/sources.list.d/"
+  command "curl -O #{mosquitto_url}#{mosquitto_repo}.list"
+end
+
+execute 'apt-update' do
+  command 'apt-get update'
+end
+
+apt_package 'mosquitto mosquitto-clients python-mosquitto'
+
+service 'mosquitto' do
+  action :stop
+end

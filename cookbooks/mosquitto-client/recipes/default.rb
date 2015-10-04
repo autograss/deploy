90b0f2e83779081c5b32e25dddcd35022c899f49
David Carlos <ddavidcarlos1392@gmail.com>
2015-10-04 19:58:29 -0300

    Creates mosquitto sub configuration file

--- cookbooks/mosquitto-client/recipes/default.rb
+++ cookbooks/mosquitto-client/recipes/default.rb
@@ -1,6 +1,24 @@
 include_recipe 'mosquitto::default'
 
-execute 'mosquitto-brokker' do
-  command "mosquitto_pub -h 192.168.50.4 -p 13000 -m teste -t 'Ola Mundo' & "
+server = data_bag_item('mosquitto','server')
+
+AUTOGRASS_CONF_DIR = "#{server['home']}/.config"
+
+clients = data_bag_item('mosquitto','pub')
+host = clients['host']
+port = clients['port']
+
+directory "#{server['home']}/.config" do
+  owner 'autograss'
+  group 'sudo'
+  mode '0755'
+  action :create
 end
 
+template "#{AUTOGRASS_CONF_DIR}/mosquitto_sub" do
+  source 'mosquitto_sub.erb'
+  mode '0750'
+  owner 'autograss'
+  group 'sudo'
+  variables({:host => host, :port => port})
+end
